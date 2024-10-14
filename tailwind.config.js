54d92fc36057844fc0215aa252dd54838c92e870
Lucas <lucas@informatique-m2i.fr>
2024-10-14 16:38:07 +0200

    update front page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ["./public/**/*.{html,js}", "./src/**/*.{ts,js,css}"],
   theme: {
     extend: {},
+    colors: {
+      'primary': '#495E57',
+      'secondary': '#F4CE14',
+      'tertiary': '#737373',
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
