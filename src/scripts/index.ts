ef6ae73575ba5383983d67bf2e58617c99096062
Lucas Ratiaray <dev@lucasratiaray.fr>
2025-01-12 20:49:28 +0100

    update

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,5 @@
 //index.ts
 
-import { displayMovies, generateStars } from "./movies-list";
 import loadComponent from "./app.js";
 
-loadComponent("./components/nav.html", "nav");
 loadComponent("./components/footer.html", "footer");
